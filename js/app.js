1a7457dbcb3c7a339125f0c931f02f73cadd9949
Reka <reka.kovacs1993@gmail.com>
2023-05-15 15:58:39 +0100

    exteension done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector("#new-item-form")
   form.addEventListener("submit", handleForm)
+
+  let button = document.createElement("button")
+  document.querySelector("body").append(button)
+  button.textContent = "Delete"
+
 })
 
 
@@ -11,16 +16,30 @@ const handleForm = function(event) {
   let title = event.target.title.value
   let author = event.target.author.value
   let category = event.target.category.value
+
   let newBook = document.createElement("div")
-  let childrenT = document.createElement("p")
-  let childrenA = document.createElement("p")
+  let childrenT = document.createElement("h1")
+  let childrenA = document.createElement("h4")
   let childrenC = document.createElement("p")
+  
+
+  newBook.classList.add("blue")
   newBook.append(childrenT, childrenA, childrenC)
+  
   childrenT.textContent = `${title}`
   childrenA.textContent = `${author}`
   childrenC.textContent = `${category}`
 
   document.querySelector("#reading-list").appendChild(newBook)
+  const delAll = document.querySelector("button")
+  delAll.addEventListener("click", deleteAll)
+
+
 
   event.target.reset()
+}
+
+const deleteAll = function () {
+  console.log("Fuck yous")
+  document.querySelector("ul").remove()
 }
